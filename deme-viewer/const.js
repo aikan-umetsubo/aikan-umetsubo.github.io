bd362c47bca99b68d82cc473c5b52a12b45eb2a0
aikan-umetsubo <yakinori21@gmail.com>
2020-01-01 02:14:51 +0900

    Modify: リールが上下に動くようにした。左右はまだ動かない

---
+++ deme-viewer/const.js
@@ -0,0 +1,13 @@
+const machines = [
+  {
+    "id": 1,
+    "label": "サンダーVリボルト",
+    "img": "http://a-project777.com/thunder_v_rebolt/img/main_spec.png",
+    "initialPosition": {
+      "left": 7,
+      "center": 9,
+      "right": 13
+    },
+    "symbolCount": 21
+  }
+]
--- deme-viewer/index.js
+++ deme-viewer/index.js
@@ -4,14 +4,19 @@ const root = document.querySelector(":root");
 class ViewModel {
   constructor(leftReel, centerReel, rightReel, leftCursor, centerCursor, rightCursor) {
     // リール
-    this.leftReel = leftReel;
-    this.centerReel = centerReel;
-    this.rightReel = rightReel;
+    this.reels = {
+      left: leftReel,
+      center: centerReel,
+      right: rightReel
+    }
 
     // カーソル
-    this.leftCursor = leftCursor;
-    this.centerCursor = centerCursor;
-    this.rightCursor = rightCursor;
+    this.cursors = {
+      left: leftCursor,
+      center: centerCursor,
+      right: rightCursor,
+      selected: "left"
+    }
 
     // コンフィグ
     this.config = new Config();
@@ -19,24 +24,22 @@ class ViewModel {
     // URLのハッシュの更新
     this.updateHash();
   }
+
   selectedReel() {
-    let reels = {
-      left: this.leftReel,
-      center: this.centerReel,
-      right: this.rightReel
-    }
-    return reels[this.cursor.select];
+    return this.reels[this.cursors.selected];
   }
+
   updateHash() {
-    window.location.hash = `${this.leftReel.hash}${this.centerReel.hash}${this.rightReel.hash}`;
+    window.location.hash = `${this.reels.left.hash}${this.reels.center.hash}${this.reels.right.hash}`;
   }
+
   init() {
     if (!window.location.hash) {
       /* TODO: hashなしの場合の初期処理 */
     } else {
-      this.leftReel.to(config.initialPosition.left);
-      this.centerReel.to(config.initialPosition.center);
-      this.rightReel.to(config.initialPosition.right);
+      this.reels.left.to(config.initialPosition.left);
+      this.reels.center.to(config.initialPosition.center);
+      this.reels.right.to(config.initialPosition.right);
     }
   }
 };
@@ -48,17 +51,21 @@ class Symbol {
     this.cssVariable = cssVariable;
     root.style.setProperty(this.cssVariable, this.position);
   }
+
   toString() {
     return `${this.cssVariable} : ${this.position}`;
   }
+
   toUp() {
     this.position = (this.position + 1) % config.symbolCount;
     root.style.setProperty(this.cssVariable, this.position);
   }
+
   toDown() {
     this.position = (this.position　- 1 + config.symbolCount) % config.symbolCount;
     root.style.setProperty(this.cssVariable, this.position);
   }
+  
   to(position) {
     this.position = (position + config.symbolCount) % config.symbolCount;
     root.style.setProperty(this.cssVariable, this.position);
@@ -73,6 +80,7 @@ class Reel {
     this.lower_symbol = lower;
     this.updateHash();
   }
+  
   toString() {
     return [
       this.upper_symbol.toString(),
@@ -80,24 +88,28 @@ class Reel {
       this.lower_symbol.toString()
     ];
   }
+  
   toUp() {
     this.upper_symbol.toUp();
     this.middle_symbol.toUp();
     this.lower_symbol.toUp();
     this.updateHash();
   }
+  
   toDown() {
     this.upper_symbol.toDown();
     this.middle_symbol.toDown();
     this.lower_symbol.toDown();
     this.updateHash();
   }
+  
   to(position) {
     this.upper_symbol.to(position);
     this.middle_symbol.to(position+1);
     this.lower_symbol.to(position+2);
     this.updateHash();
   }
+  
   updateHash() {
     this.hash = String(this.upper_symbol.position+1).padStart(2, "0");
   }
@@ -121,18 +133,22 @@ class Cursors {
     this.cursors[1].hide();
     this.cursors[2].hide();
   }
+  
   current() {
     return _current;
   }
+  
   toString() {
     return ["left", "center", "right"][this._current];
   }
+  
   toLeft() {
     /* TODO:今のカーソルをhiddenに */
     this._current = (this._current + 2) % 3;
     this.current = this.toString();
     /* TODO:新しいカーソルを表示 */
   }
+  
   toRight() {
     /* TODO:今のカーソルをhiddenに */
     this._current = (this._current + 1) % 3;
@@ -140,18 +156,22 @@ class Cursors {
     /* TODO:新しいカーソルを表示 */
   }
 }
+
 class Cursor {
   constructor(upperCssVariable, lowerCssVariable) {
     this.upperCssVariable = upperCssVariable;
     this.lowerCssVariable = lowerCssVariable;
   }
+  
   toString() {
     return `${this.upperCssVariable} - ${this.lowerCssVariable}`;
   }
+  
   hide() {
     root.style.setProperty(this.upperCssVariable, 'hidden');
     root.style.setProperty(this.lowerCssVariable, 'hidden');
   }
+  
   show() {
     root.style.setProperty(this.upperCssVariable, 'visible');
     root.style.setProperty(this.lowerCssVariable, 'visible');
@@ -170,6 +190,7 @@ class Hash {
     }
     window.location.hash = `${this.left.hash}${this.center.hash}${this.right.hash}`;
   }
+  
   isValid(left, center, right) {
     return !left && String(left).match(/0[1-9]|1[1-9]|2[0-1]/)
            &&
@@ -212,7 +233,9 @@ const viewModel = new ViewModel(
     new Symbol(config.initialPosition.right+1, "--right-middle-reel-position"),
     new Symbol(config.initialPosition.right+2, "--right-lower-reel-position")
   ),
-  new Cursors(),
+  new Cursor("--upper-left-cursor-display", "--lower-left-cursor-display"),
+  new Cursor("--upper-center-cursor-display", "--lower-center-cursor-display"),
+  new Cursor("--upper-right-cursor-display", "--lower-right-cursor-display")
 );
 
 // ページの読み込み後にビューモデルを初期化
