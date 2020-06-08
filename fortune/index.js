0748cf0cc5a57e97f1e9c96136790563ea17de2e
aikan-umetsubo <yakinori21@gmail.com>
2020-06-09 07:06:27 +0900

    Modify: design change * changed the font color and the background color * changed the font to monospace ones * added terminal-like text * changed the layout

--- fortune/index.js
+++ fortune/index.js
@@ -1,9 +1,15 @@
 window.onload = () => {
     const request = new XMLHttpRequest();
     request.onload = (e) => {
+        // parse the response to get fortune
         const response = JSON.parse(e.target.responseText);
-        const fortune = document.querySelector("article#fortune");
+
+        // display the fortune
+        const fortune = document.querySelector("p#line2");
         fortune.innerHTML = response.message;
+
+        // display the commandline
+        document.querySelector('p#line3').classList.remove("hidden");
     }
     request.open("GET", "https://9lyxd9jwml.execute-api.us-east-1.amazonaws.com/dev");
     request.send();
