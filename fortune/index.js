70296b1ce4e0f744d2b345931e8c34a7789cf6ef
aikan-umetsubo <yakinori21@gmail.com>
2020-06-09 06:22:52 +0900

    Modify: added the feature to load fortune from the api

--- fortune/index.js
+++ fortune/index.js
@@ -1,4 +1,10 @@
-const fortune = document.querySelector("article#fortune");
 window.onload = () => {
-
+    const request = new XMLHttpRequest();
+    request.onload = (e) => {
+        const response = JSON.parse(e.target.responseText);
+        const fortune = document.querySelector("article#fortune");
+        fortune.innerHTML = response.message;
+    }
+    request.open("GET", "https://9lyxd9jwml.execute-api.us-east-1.amazonaws.com/dev");
+    request.send();
 }
