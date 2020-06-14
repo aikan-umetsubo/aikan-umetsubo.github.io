f576292089e142c685ea7f6e9547a9e0125a4193
aikan-umetsubo <yakinori21@gmail.com>
2020-06-14 15:41:18 +0900

    いろいろ粗はあるが、とりあえず大当りシミュレーターをコミット
    自分で気づいてる粗の一覧
    ・実質継続率しか見てないので、確変機なのに1連で終わることがある
    ・デザインがちょっと適当すぎ
    ・全てをSimulatorクラスに突っ込んでるので責務が曖昧
    ・output関数もなんか適当な感じ

---
+++ oatari_simu/index.js
@@ -0,0 +1,111 @@
+class Simulator {
+  constructor(bonusProb, enterProb, continueProb) {
+    this.bonusProb = 1.0 / bonusProb;
+    this.enterProb = enterProb / 100.0;
+    this.continueProb = continueProb / 100.0;
+    this.mt = new MersenneTwister();
+
+    this.isBonus = false;
+    this.isKakuhen = false;
+  }
+
+  playANormalGame() {
+    if (this.mt.genrand_real1() <= this.bonusProb) {
+      this.isBonus = true;
+      this.isKakuhen = this.enterKakuhen();
+    }
+  }
+
+  // 確変に突入するか判定
+  enterKakuhen() {
+    return this.mt.genrand_real1() <= this.enterProb;
+  }
+
+  // 確変から転落するまで回す
+  playUntilTenraku() {
+    let bonusCount = 1;
+
+    while (true) {
+      let endKakuhen = this.mt.genrand_real1() > this.continueProb;
+      if (endKakuhen) {
+        break;
+      }
+      bonusCount++;
+    }
+
+    this.isBonus = false;
+    this.isKakuhen = false;
+
+    return bonusCount;
+  }
+
+  // 決まった通常ゲーム数回す
+  play(maxNormalGames) {
+    const results = [];
+    let totalGames = 0;
+    let games = 0;
+
+    while (totalGames <= maxNormalGames) {
+      games++;
+      totalGames++;
+      this.playANormalGame();
+
+      if (this.isBonus && !this.isKakuhen) {
+        // 通常大当り
+        results.push({
+          games: games,
+          type: "normal",
+          count: 1
+        });
+        games = 0;
+        this.isBonus = false;
+
+      } else if (this.isBonus && this.isKakuhen) {
+        // 確変大当り
+        let count = this.playUntilTenraku();
+        results.push({
+          games: games,
+          type: "kakuhen",
+          count: count
+        });
+        games = 0;
+      }
+    }
+
+    results.push({
+      games: games,
+      type: "finish",
+      count: null
+    })
+
+    return results;
+  }
+}
+
+const output = (result) => {
+  if (result.type === 'normal') {
+    return `${result.games}G 通常<br>`;
+
+  } else if (result.type === 'kakuhen') {
+    return `${result.games}G 確変 ${result.count}連<br>`;
+
+  } else {
+    return `${result.games}G やめ<br>`;
+  }
+}
+
+window.onload = () => {
+  document.querySelector('button#start').onclick = () => {
+    document.querySelector('div#result').innerHTML  = '';
+
+    const simulator = new Simulator(
+      document.querySelector('input#bonus1').value,
+      document.querySelector('input#enter').value,
+      document.querySelector('input#continue').value
+    );
+
+    simulator.play(2000).forEach((result) => {
+      document.querySelector('div#result').innerHTML += output(result);
+    });
+  };
+};
